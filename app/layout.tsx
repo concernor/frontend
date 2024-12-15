6284870aa8601c032536c33f785ffc4430a5927b
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-12-15 22:23:49 +0300

    add landing

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "The Concernooooorrrrr",
+  description:
+    "very concerned. wants you to know he is concerned. has a bag that can alleviate your concerns.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,87 @@
+import { Button } from "@/components/ui/button";
+import { SiGithub, SiX } from "@icons-pack/react-simple-icons";
+import { ExternalLink } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">The Concernooooorrrrr</h1>
+
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li>very concerned.</li>
+          <li>wants you to know he is concerned </li>
+          <li>has a bag that can alleviate your concerns</li>
+        </ol>
+
+        <div className="w-full flex justify-between items-center flex-col sm:flex-row">
+          <Button asChild className="mr-4">
+            <a href="https://x.com/concernor">
+              Get Concerned
+              <ExternalLink className="w-4 h-4 ml-2" />
+            </a>
+          </Button>
+          <div className="flex gap-1">
+            <Button asChild size="icon">
+              <a href="https://x.com/concernor">
+                <SiX className="w-4 h-4" />
+              </a>
+            </Button>
+            <Button asChild size="icon">
+              <a href="https://github.com/concernor">
+                <SiGithub className="w-4 h-4" />
+              </a>
+            </Button>
+          </div>
+        </div>
+      </main>
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer> */}
+    </div>
+  );
+}

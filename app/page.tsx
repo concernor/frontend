5db5473aeb88fd7332e0486f6bb2ca2574167156
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-12-16 00:52:27 +0300

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { SiGithub, SiX } from "@icons-pack/react-simple-icons";
-import { ExternalLink } from "lucide-react";
 
 export default function Home() {
   return (
@@ -34,48 +33,11 @@ export default function Home() {
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Lore? || Disclosure || Roadmap || Terms || FAQ
         </a>
       </footer> */}
     </div>

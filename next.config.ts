de8316de2b46313523fdce1ab114b8b1485f906e
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-12-18 15:00:17 +0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;

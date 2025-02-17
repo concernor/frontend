17af9096ae5ea6a023b314e56ff5ac43f6d05e06
cagdotin <13513097+cagdotin@users.noreply.github.com>
2025-02-17 21:30:31 +0100

    dependency & config updates

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,24 +1,33 @@
 
-
-
-
+// coin details.
 export const coin = {
     ticker: "CONCERN",
     address: "0x8f5a590be0b27feb5bffaab82bbf31d3c7efe222",
     chains: ["base"]
 }
 
-export const links = {
+// Static pages
+export const paths = {
     origins: "/origins",
+    manifesto: "/manifesto",
+    terms: "/terms",
+    privacy: "/privacy",
+}
+
+// All Links including static pages
+export const links = {
+    ...paths,
     docs: "https://concernor.github.io/docs",
     flaunch: "https://flaunch.gg/base/coin/0x8F5A590Be0B27fEb5bFfaab82BBF31d3c7EFe222"
 }
 
+// Social accounts of concernor.
 export const socials = {
     x: "https://x.com/concernor",
     farcaster: "https://warpcast.com/concernor.eth",
     paragraph: "https://paragraph.xyz/@concernor",
     telegram: "https://t.me/+agpEOssgKQNkNjRk",
-    github: "https://github.com/concernor"
+    github: "https://github.com/concernor",
+    discord: "https://discord.gg/2FnrMkH9Dm"
 }
 
